f6ff247c3f7392b92ac48f39be7020979a302441
Eric Guo <eric.guo@sandisk.com>
2016-01-09 01:42:38 +0800

    Touch the web immediately after restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,14 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :curl, '-s -D - aki-aikido.cn -o /dev/null'
+    end
+  end
+
+  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
